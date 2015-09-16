f619a58318bb0580a6e8a247151895915ea3ef5a
Elliott Hughes-Taylor <ehughestaylor@Elliotts-MacBook-Pro.local>
2015-09-16 18:27:56 -0400

    c

--- Gruntfile.js
+++ Gruntfile.js
@@ -461,4 +461,21 @@ module.exports = function (grunt) {
     'test',
     'build'
   ]);
+      copy: {
+        dist: {
+            files: [{
+                expand: true,
+                dest: '<%= yeoman.dist %>',
+                cwd: 'heroku',
+                src: '*',
+                rename: function (dest, src) {
+                    var path = require('path');
+                    if (src === 'distpackage.json') {
+                        return path.join(dest, 'package.json');
+                    }
+                    return path.join(dest, src);
+                }
+            }]
+        }
+    }
 };
